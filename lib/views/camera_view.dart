4d9003408ce815d4fe67d2d0f91489fd86f39770
romaine877 <romaine877@gmail.com>
2022-01-27 23:58:56 -0500

    camera flash functionality implemented

--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_scan_app/widgets/modal.dart';
 
 class CameraView extends StatefulWidget {
   const CameraView({Key? key}) : super(key: key);
@@ -16,41 +17,40 @@ class _CameraViewState extends State<CameraView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onViewCreated,
-              overlay: QrScannerOverlayShape(
-                borderColor: Colors.indigo,
-                borderRadius: 10,
-                borderLength: 30,
-                borderWidth: 10,
-                cutOutSize: MediaQuery.of(context).size.width * 0.6,
-              ),
-              onPermissionSet: (control, permission) =>
-                  _onPermissionSet(context, control, permission),
-            ),
-          ),
-          res != null
-              ? Expanded(
-                  flex: 1,
-                  child: Center(
-                    child: Text(
-                      res?.code ?? '',
-                      style: const TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                )
-              : const Expanded(
-                  flex: 1,
-                  child: Center(child: CircularProgressIndicator()),
-                ),
+           FloatingActionButton.large(
+        child: const Icon(Icons.camera_alt),
+        onPressed: () {
+          controller?.toggleFlash();
+        },
+        heroTag: null,
+      ),
+       FloatingActionButton.large(
+        child: const Icon(Icons.flash_on),
+            
+        onPressed: () async{
+          await controller?.toggleFlash();
+          setState(() {});
+        },
+        heroTag: null,
+      ),
         ],
+      ),
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Colors.indigo,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: MediaQuery.of(context).size.width * 0.6,
+        ),
+        onPermissionSet: (control, permission) =>
+            _onPermissionSet(context, control, permission),
       ),
     );
   }
@@ -70,12 +70,17 @@ class _CameraViewState extends State<CameraView> {
       this.controller = controller;
     });
     controller.scannedDataStream.first.then((scanData) {
-      Navigator.pop(context);
-       setState(() {
+      showModalBottomSheet(
+          context: context,
+          barrierColor: Color(0x01000000),
+          builder: (context) {
+            return QrModal(qrData: scanData.code);
+          });
+      setState(() {
         res = scanData;
       });
+      controller.pauseCamera();
     });
-    
   }
 
   @override
